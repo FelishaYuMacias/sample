924f206cb16da23df2e8bc788e4a1d05a41544fe
Felisha <felisha.j.yu@gmail.com>
2022-12-01 11:08:20 -0800

    updated home name

---
+++ src/components/NavTabs.js
@@ -0,0 +1,57 @@
+import React from 'react';
+
+// TODO: Add a comment explaining how we are able to extract the key value pairs from props
+  //it was exported from Portfolio container and desctructured
+
+function NavTabs({ currentPage, handlePageChange }) {
+  return (
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <a
+          href="#home"
+          onClick={() => handlePageChange('Home')}
+          //*  TODO: BONUS: Add a comment explaining what kind of operator this is and what it is checking for
+              //this is checking to see that the current page is "Home" and updating the className from inactive to active
+          className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
+        >
+          Home
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#about"
+          onClick={() => handlePageChange('About')}
+          //  TODO: Add a comment explaining what this logic is doing
+//this is checking to see that the current page is "About" and updating the className from inactive to active
+          className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+        >
+          About
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#blog"
+          onClick={() => handlePageChange('Blog')}
+          //  TODO: Add a comment explaining what this logic is doing
+//this is checking to see that the current page is "Blog" and updating the className from inactive to active
+          className={currentPage === 'Blog' ? 'nav-link active' : 'nav-link'}
+        >
+          Blog
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#contact"
+          //  TODO: Add a comment explaining what this logic is doing
+//this is checking to see that the current page is "Contact" and updating the className for that Nav link from inactive to active
+          onClick={() => handlePageChange('Contact')}
+          className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+        >
+          Contact
+        </a>
+      </li>
+    </ul>
+  );
+}
+
+export default NavTabs;

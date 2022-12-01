2b3e022762e6d563717020589d0c68a47745f8d4
Felisha <felisha.j.yu@gmail.com>
2022-12-01 11:05:42 -0800

    Updates

---
+++ precache-manifest.612a5baac02a92a12b015e2f106945e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b3715e0170e1e963062a",
+    "url": "/sample/static/js/main.b3715e01.chunk.js"
+  },
+  {
+    "revision": "904988be85cd23bbd78c",
+    "url": "/sample/static/css/1.2b9294c6.chunk.css"
+  },
+  {
+    "revision": "904988be85cd23bbd78c",
+    "url": "/sample/static/js/1.904988be.chunk.js"
+  },
+  {
+    "revision": "455bb57d6ec0ff8acc00",
+    "url": "/sample/static/js/runtime~main.455bb57d.js"
+  },
+  {
+    "revision": "6fa1225a653568c69ce1e646e706439a",
+    "url": "/sample/index.html"
+  }
+];
